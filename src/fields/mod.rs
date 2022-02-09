baabf660e93792cc00c2f14d6154909ce7f3b2fc
Giacomo Fenzi <giacomofenzi@outlook.com>
2022-02-09 21:47:18 +0100

    Move tests

--- src/fields/mod.rs
+++ src/fields/mod.rs
@@ -5,6 +5,10 @@ use std::ops::{Add, Mul, Neg, Sub};
 use num::{BigUint, Integer};
 use rand::RngCore;
 
+// This is actually need for the macro for tests to compile
+#[allow(unused_imports)]
+use paste::paste;
+
 pub trait Field:
     Clone
     + PartialEq
@@ -28,7 +32,7 @@ pub trait Field:
         self == &Self::one()
     }
 
-    fn invert(self) -> Option<Self>;
+    fn invert(&self) -> Option<Self>;
 
     fn characteristic() -> BigUint;
 
@@ -104,4 +108,88 @@ where
         let p = divide_and_conquer_impl(base, (i - I::one()) / two, id, add);
         add(base.clone(), add(p.clone(), p))
     }
+}
+
+#[macro_export]
+macro_rules! field_tests {
+    ($ff:ident) => {
+        paste! {
+            #[cfg(test)]
+            mod [< $ff:snake _tests >] {
+                use super::$ff;
+                use crate::fields::Field;
+                use crate::primes::miller_rabin_with_randomness;
+                use num::BigUint;
+                use rand::SeedableRng;
+
+                #[test]
+                fn obvious_things() {
+                    assert!($ff::zero().is_zero());
+                    assert!($ff::one().is_one());
+                    assert!(!$ff::zero().is_one());
+                    assert!(!$ff::one().is_zero());
+                    assert!($ff::zero() != $ff::one());
+                    assert!($ff::characteristic() >= BigUint::from(2u8));
+                }
+
+                #[test]
+                fn modulo_is_prime() {
+                    const ROUNDS: usize = 1000;
+                    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(42);
+                    assert!(
+                        miller_rabin_with_randomness(&mut rng, $ff::characteristic(), ROUNDS)
+                            .is_prime()
+                    );
+                }
+
+                #[test]
+                fn additive_identies() {
+                    const NUM_ELEMENTS: usize = 1000;
+                    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(42);
+                    assert_eq!($ff::zero() + $ff::zero(), $ff::zero());
+                    for _ in 0..NUM_ELEMENTS {
+                        let el = $ff::random(&mut rng);
+                        assert_eq!(el, $ff::zero() + &el);
+                        assert_eq!(el, el.clone() + $ff::zero());
+                        assert_eq!(el.clone() - el.clone(), $ff::zero());
+
+                        let el2 = $ff::random(&mut rng);
+                        assert_eq!(el.clone() + el2.clone(), el2 + el);
+                    }
+                }
+
+                #[test]
+                fn multiplicative_identies() {
+                    const NUM_ELEMENTS: usize = 1000;
+                    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(42);
+                    assert!($ff::zero().invert().is_none());
+                    assert_eq!($ff::one() * $ff::one(), $ff::one());
+                    for _ in 0..NUM_ELEMENTS {
+                        let el = $ff::random_non_zero(&mut rng);
+                        assert_eq!(el, $ff::one() * &el);
+                        assert_eq!(el, el.clone() * $ff::one());
+                        assert!(el.invert().is_some());
+                        let inv = el.invert().unwrap();
+                        assert_eq!(el.clone() * inv.clone(), $ff::one());
+                        let el2 = $ff::random(&mut rng);
+                        assert_eq!(el.clone() * el2.clone(), el2 * el);
+                    }
+                }
+
+                #[test]
+                fn distributivity() {
+                    const NUM_ELEMENTS: usize = 1000;
+                    let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(42);
+                    for _ in 0..NUM_ELEMENTS {
+                        let (a, b, c) = (
+                            $ff::random(&mut rng),
+                            $ff::random(&mut rng),
+                            $ff::random(&mut rng),
+                        );
+                        assert_eq!(a.clone() * (b.clone() + c.clone()), a.clone() * b + a * c);
+                    }
+                }
+            }
+        }
+    };
 }
--- src/fields/primefields.rs
+++ src/fields/primefields.rs
@@ -1,4 +1,5 @@
 use super::Field;
+use crate::field_tests;
 use lazy_static::lazy_static;
 use num::bigint::RandBigInt;
 use num::{BigUint, One, Zero};
@@ -95,7 +96,7 @@ macro_rules! field_generate {
                 Self::new_unchecked(r.gen_biguint_range(&BigUint::zero(), &*[<$ff:upper _MODULO>]))
             }
 
-            fn invert(self) -> Option<Self> {
+            fn invert(&self) -> Option<Self> {
                 if self.el.is_zero() {
                     return None;
                 }
@@ -116,47 +117,7 @@ macro_rules! field_generate {
         }
                 }
 
-        #[cfg(test)]
-        mod tests {
-            use super::$ff;
-            use crate::fields::Field;
-            use crate::primes::miller_rabin_with_randomness;
-            use num::BigUint;
-            use rand::SeedableRng;
-
-            #[test]
-            fn obvious_things() {
-                assert!($ff::zero().is_zero());
-                assert!($ff::one().is_one());
-                assert!(!$ff::zero().is_one());
-                assert!(!$ff::one().is_zero());
-                assert!($ff::zero() != $ff::one());
-                assert!($ff::characteristic() >= BigUint::from(2u8));
-            }
-
-            #[test]
-            fn modulo_is_prime() {
-                const ROUNDS: usize = 1000;
-                let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(42);
-                assert!(
-                    miller_rabin_with_randomness(&mut rng, $ff::characteristic(), ROUNDS)
-                        .is_prime()
-                );
-            }
-
-            #[test]
-            fn additive_identies() {
-                const NUM_ELEMENTS: usize = 1000;
-                let mut rng = rand_chacha::ChaCha20Rng::seed_from_u64(42);
-                assert_eq!($ff::zero() + $ff::zero(), $ff::zero());
-                for _ in 0..NUM_ELEMENTS {
-                    let el = $ff::random(&mut rng);
-                    assert_eq!(el, $ff::zero() + &el);
-                    assert_eq!(el, el.clone() + $ff::zero());
-                    assert_eq!(el.clone() - el, $ff::zero());
-                }
-            }
-        }
+        field_tests!($ff);
     };
 }
 
